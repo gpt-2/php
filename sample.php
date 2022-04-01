6a3f302a31bee16363114eef1a67cd48a6e62c62
GPT-2 <102778950+gpt-2@users.noreply.github.com>
2022-04-01 07:35:08 +0300

    Update sample.php

--- sample.php
+++ sample.php
@@ -2,8 +2,8 @@
 include_once "src/gpt2com.php";
 
 $gpt2= new gpt2com;
-$gpt2->user_id="YOUR_USER_ID";
-$gpt2->api_key="YOUR_API_KEY";
+$gpt2->user_id="YOUR_USER_ID";   // <---- Replace with your gpt-2.com User ID !!!
+$gpt2->api_key="YOUR_API_KEY";   // <---- Replace with your gpt-2.com API key !!!
 
 echo $gpt2->generate("Online casino gambling, in the past few years, has become a multi billion dollar industry");
 echo "\n";
